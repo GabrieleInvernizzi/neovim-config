bcb5d31391004364f7a55781cbd0cf5ce39947f0
Gabriele Invernizzi <gabrieleinvernizzi4@gmail.com>
2022-09-18 15:38:14 +0200

    Added init.lua.

---
+++ init.lua
@@ -0,0 +1,12 @@
+
+vim.opt.number = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false 	-- check
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
